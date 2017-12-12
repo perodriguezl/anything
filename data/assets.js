79c0426b6b1d0c6e44f95edb63f06b78ae105b03
Pedro <perodriguez_ext@amco.mx>
2017-12-12 13:56:21 -0300

    second commit

---
+++ data/assets.js
@@ -0,0 +1,28 @@
+var db = {
+	'3 1/8" EWR Top Assembly' :
+	{
+		'120150174': { 'name': 'O-Ring 568-214 NIT 90DR C67 (Hanger, Receiver and Transmitter Inserts)', 'quantity': 8},
+		'120141941': { 'name': 'O-Ring 568-225 NIT 90DR C67 (Hanger)', 'quantity': 4},
+		'120141502': { 'name': 'O-R,568-228,NIT,90 DURO C67 (Antenna Collar)', 'quantity': 4},
+		'120173417': { 'name': 'O-Ring 568-142 90DR NIT C67 (Transmitter Insert)', 'quantity': 1},
+		'120141347': { 'name': 'O-Ring 568-122 NIT 90DR C67 (Hard Connect)', 'quantity': 2},
+		'120139700': { 'name': 'O-Ring 568-115 NIT 90DR C67 (SWRO)', 'quantity': 2},
+		'120139695': { 'name': 'O-Ring 568-120 NIT 90DR C67 (Shipping Plug)', 'quantity': 2},
+	},
+	'4 3/4" ADR Top Assembly' : 
+	{
+		"120139704" : { "name" : 'O-Ring 568-347 NIT, C67 90DR (Antenna Collar and Battery, Transmitter, and Receiver Inserts)', "quantity": 10, },
+		"120139702" : { "name" : 'O-Ring 568-334 NIT, C67 90DR (Battery, Transmitter, and Receiver Inserts)', "quantity":	6, },
+		"120161884" : { "name" : 'O-Ring 568-245, NIT C67 90DR (Battery, Transmitter, and Receiver Inserts)', "quantity":	6, },
+		"120139699" : { "name" : 'O-Ring,568-013,NIT,90 DR C67 (Dump Cover)', "quantity":	4, },
+		"120139700" : { "name" : 'O- Ring, 568-115 NIT 90DR C67 (SWRO)', "quantity":	2, },
+	},
+	'6 3/4" ADR Top Assembly': 
+	{
+		'120139704': { 'name': 'O-Ring 568-347 NIT, C67 90DR (Antenna Collar and Battery, Transmitter, and Receiver Inserts)', 'quantity': 10 },
+		'120139702': { 'name': 'O-Ring 568-334 NIT, C67 90DR (Battery, Transmitter, and Receiver Inserts)', 'quantity': 6 },
+		'120161884': { 'name': 'O-Ring 568-245, NIT C67 90DR (Battery, Transmitter, and Receiver Inserts)', 'quantity': 6 },
+		'120139699': { 'name': 'O-Ring,568-013,NIT,90 DR C67 (Dump Cover)', 'quantity': 4 },
+		'120139700': { 'name': 'O- Ring, 568-115 NIT 90DR C67 (SWRO)', 'quantity': 2 },
+	},
+};
